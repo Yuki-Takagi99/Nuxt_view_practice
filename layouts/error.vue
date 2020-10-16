88ab4b6b50b283e0f118ea5e59baece505c912d7
yuki takagi <yuki99takagi@gmail.com>
2020-10-16 15:15:39 +0900

    first commit

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">404Error!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  props: ['error']
+}
+</script>
+
+<style scoped>
+
+</style>
